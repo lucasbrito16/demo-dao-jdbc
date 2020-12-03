1d7bcdb81bd78a8e8f7e7177abd9c4a3df465209
Lucas Brito <lucasgomesbrito@gmail.com>
2020-12-02 22:56:51 -0300

    DepartmentDao and SellerDao interfacesDepartmentDao and SellerDao interfaces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,11 @@
 package application;
 
+import java.awt.List;
+import java.util.Comparator;
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -16,6 +20,10 @@ public class Program {
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 		
 		System.out.println(seller);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao(); 
+	
+		
 
 	}
 
